05925472da0747ace464e9e8017ce20d867d1107
Marinela Rajkovic <marinela.rajkovic29@gmail.com>
2025-11-04 15:13:00 +0100

    Learning about Boolean

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-let firstCard = 2
+/* let firstCard = 2
 let secondCard = 8
 
 let sum = firstCard + secondCard
@@ -10,7 +10,52 @@ if (sum < 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")  
 } else {
     console.log("You're out of the game! 😭")
+} */
+
+
+
+
+
+
+/* let age = 22
+
+if (age < 22) {
+   console.log("You can not enter the club!") 
+} else {
+    console.log("Welcome!")
 }
+ */
 
 
 
+/* let age = 100
+
+    if (age < 100) {
+        console.log("Not elegible")
+    } else if (age === 100) {
+         console.log("Here is your birthday card from the King!")
+    } else {
+        console.log("Not elegible, you have already gotten one")
+    }
+ */
+
+
+
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+
+let isAlive = true 
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+    
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+        hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+    isAlive = false
+}
